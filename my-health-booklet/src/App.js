8ae7247e3f1175c9bdee4a1f0686985e02dc40aa
Soruki <rubysk@iitk.ac.in>
2022-04-10 14:39:00 +0530

    apcoming appointment

--- my-health-booklet/src/App.js
+++ my-health-booklet/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Register from './pages/Register';
 import AppRoutes from './Routes/AppRoutes';
+import UpcomingAppointment from './pages/UpcomingAppointment';
 
 function App() {
   return (
     // <AppRoutes />  
-    <Register />  
+    // <Register />  
+    <UpcomingAppointment />
   );
 }
 
